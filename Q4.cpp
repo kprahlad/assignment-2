5e055829cb1d5b4468bb42ab719a7dff5126cb1b
kprahlad <31153973+kprahlad@users.noreply.github.com>
2017-08-30 15:16:17 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main()
 {
-  int days,years,months,d;
+  int days,years,weeks,d;
   cin>>days;
   years = days/365;
   days = days%365;
-  months = days/30;
-  days = days%30;
+  weeks = days/7;
+  days = days%7;
   d=days;
   cout<<years<<months<<d;
   return 0;
