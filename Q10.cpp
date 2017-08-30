6fe74e6338dd1a904c1f37342f45cee25af68fd5
kprahlad <31153973+kprahlad@users.noreply.github.com>
2017-08-30 15:11:00 +0530

    Create Q10.cpp

---
+++ Q10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+  float m1,m2,m3,m4,m5,tot,avg,per;
+  cin>>m1,m2,m3,m4,m5;
+  tot=m1+m2+m3+m4+m5;
+  avg=tot/5.0;
+  per=tot/5.0;
+  cout<<tot<<avg<<per;
+  return 0;
+}
